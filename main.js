988b794682f272ccbebbf9f18e91ed72c509bde5
Fatih Arda Zengin <fatihardazengin@gmail.com>
2022-03-10 14:10:30 +0300

    Updated the width

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = window.innerWidth - margin.left - margin.right -((window.innerWidth/100)*20)
+    width = window.innerWidth - margin.left - margin.right -((window.innerWidth/100)*15)
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
